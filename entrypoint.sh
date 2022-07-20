a05d3fb12d96bbbb342706aac38c52bf58f8a1b3
Chris Buckley <chris@cmbuckley.co.uk>
2022-07-20 23:59:29 +0100

    -jar

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ mv "$source_dir"/*.puml "$tmp_dir"
 
 echo "Generating .png images"
 
-java /usr/local/plantuml/plantuml.jar "$tmp_dir"/*.puml
+java -jar /usr/local/plantuml/plantuml.jar "$tmp_dir"/*.puml
 
 echo "Moving C4 images to '$target_dir'"
 
